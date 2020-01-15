52f592c4f74eada7d7ad7f10a5ec94430dda7d39
Maksudur Rahman Maruf <maruf.bsnm@gmail.com>
2020-01-15 21:32:37 +0600

    First Commit

---
+++ ELibrary/src/com/javatpoint/dao/DB.java
@@ -0,0 +1,14 @@
+package com.javatpoint.dao;
+
+import java.sql.*;
+
+public class DB {
+public static Connection getCon(){
+	Connection con=null;
+	try{
+		Class.forName("com.mysql.cj.jdbc.Driver");
+	    con = DriverManager.getConnection("jdbc:mysql://localhost:3306/elibrary","root","123456");
+	}catch(Exception e){System.out.println(e);}
+	return con;
+}
+}
